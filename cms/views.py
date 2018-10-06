1d3478776f4bd93cac445c0f20e802ce6d2b2c29
yuto <na2741010@gmail.com>
2018-10-06 14:04:35 +0900

    python

--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from cms.models import Book
+from cms.forms import BookForm
 
 # Create your views here.
 
@@ -14,9 +15,26 @@ def book_list(request):
 
 def book_edit(request, book_id=None):
     """書籍の編集"""
-    return HttpResponse('書籍の編集')
+    #return HttpResponse('書籍の編集')
+    if book_id:  # book_id が指定されている (修正時)
+        book = get_object_or_404(Book, pk=book_id)
+    else:  # book_id が指定されていない (追加時)
+        book = Book()
 
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)  # POST された request データからフォームを作成
+        if form.is_valid():  # フォームのバリデーション
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:  # GET の時
+        form = BookForm(instance=book)  # book インスタンスからフォームを作成
+
+    return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
 
 def book_del(request, book_id):
     """書籍の削除"""
-    return HttpResponse('書籍の削除')
+    #return HttpResponse('書籍の削除')
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('cms:book_list')
