c021b9e2caf5b4c68dd79ee8acc85b6a82ada572
yuto <na2741010@gmail.com>
2018-10-06 12:14:39 +0900

    python

--- cms/admin.py
+++ cms/admin.py
@@ -3,5 +3,21 @@ from cms.models import Book, Impression
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Impression)
+#admin.site.register(Book)
+#admin.site.register(Impression)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'publisher', 'page',)  # 一覧に出したい項目
+    list_display_links = ('id', 'name',)  # 修正リンクでクリックできる項目
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class ImpressionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment',)
+    list_display_links = ('id', 'comment',)
+    raw_id_fields = ('book',)   # 外部キーをプルダウンにしない（データ件数が増加時のタイムアウトを予防）
+
+
+admin.site.register(Impression, ImpressionAdmin)
